3e800e69470996db34ac4fcbe66bc46a92beb3c7
q <787025321@qq.com>
2023-04-05 15:08:44 +0800

    fix: state

--- web/src/state/hooks/useGetResultsState.ts
+++ web/src/state/hooks/useGetResultsState.ts
@@ -1,6 +1,7 @@
 
 import { useSelector } from 'react-redux';
 import { AppState } from '../redux/reducers/rootReducer';
+import { ResultsState } from '../redux/reducers/results';
 export const useGetResultsState = () => {
-  return useSelector((state: AppState) => state.results);
+  return useSelector((state: AppState) => state.results as ResultsState);
 };
