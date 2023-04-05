d6d1b42f3f2c576c3bd15e1e39f451e38bc3184a
q <787025321@qq.com>
2023-04-05 14:45:55 +0800

    fix: ts

--- web/src/state/hooks/useDatabendDispatch.ts
+++ web/src/state/hooks/useDatabendDispatch.ts
@@ -1,6 +1,9 @@
 import { Dispatch } from 'react';
 import { useDispatch } from 'react-redux';
-const useDatabendDispatch = <T>()=> {
+
+const useDatabendDispatch = <T>() => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   return useDispatch<Dispatch<T>>();
 };
 export default useDatabendDispatch;
